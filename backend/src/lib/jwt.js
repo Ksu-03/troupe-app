1da39e01a8fc556f83e7ed7264d4b7dba2c7d224
Ksu-03 <kksu99319@gmail.com>
2026-03-22 20:37:52 +0300

    Create jwt.js

---
+++ backend/src/lib/jwt.js
@@ -0,0 +1,17 @@
+const jwt = require('jsonwebtoken');
+
+const JWT_SECRET = process.env.JWT_SECRET;
+
+function generateToken(userId) {
+  return jwt.sign({ userId }, JWT_SECRET, { expiresIn: '30d' });
+}
+
+function verifyToken(token) {
+  try {
+    return jwt.verify(token, JWT_SECRET);
+  } catch (error) {
+    return null;
+  }
+}
+
+module.exports = { generateToken, verifyToken };
