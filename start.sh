f3281beda0c0b4eeb9f8d55abb458c00ff948d7a
Ksu-03 <kksu99319@gmail.com>
2026-03-23 21:36:35 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+set -e
+echo "Starting Troupe Backend..."
 cd backend
+echo "Installing dependencies..."
 npm install
+echo "Generating Prisma client..."
+npx prisma generate
+echo "Starting server..."
 npm start
